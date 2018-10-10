b6b331a0b89269882a513a098c8655e25d1d7ae4
Daniel <danro880@student.liu.se>
2018-10-10 20:09:46 +0200

    Missing project renaming fixed

--- fadderjobb/wsgi.py
+++ fadderjobb/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "faddersystem.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fadderjobb.settings")
 
 application = get_wsgi_application()
