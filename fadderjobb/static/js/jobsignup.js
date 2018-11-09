6180da31a424d3e088cfca0f8de2b487ed50d254
Daniel <danro880@student.liu.se>
2018-11-09 09:46:47 +0100

    Filter visualisation stay when refreshing

--- fadderjobb/static/js/jobsignup.js
+++ fadderjobb/static/js/jobsignup.js
@@ -23,4 +23,28 @@ $(function () {
             $caret.css({opacity: 0});
         }
     });
+
+    initialMoveCaret();
 });
+
+function initialMoveCaret() {
+    $(".filter-checkbox").find("input[type=checkbox]:checked").each(function () {
+        const $this = $(this);
+        const $container = $this.parents(".filter-checkbox-container");
+        const $label = $this.siblings(".form-check-label");
+        const $caret = $container.find(".filter-checkbox-caret");
+
+        $caret.toggleClass("noanimate");
+
+        $caret.offset($label.offset());
+        $caret.css({width: $label.outerWidth(), opacity: 1});
+
+        if ($label.hasClass("form-check-label-yes")) {
+            $caret.css({backgroundColor: "#63bf69"});
+        } else {
+            $caret.css({backgroundColor: "#ff4c2f"});
+        }
+
+        $caret.toggleClass("noanimate");
+    });
+}
