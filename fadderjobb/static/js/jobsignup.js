4fc87541c7a0cf5d1ab159fd088b29bb2301bf40
Daniel <danro880@student.liu.se>
2018-11-09 13:00:56 +0100

    Minor bugfix

--- fadderjobb/static/js/jobsignup.js
+++ fadderjobb/static/js/jobsignup.js
@@ -6,14 +6,14 @@ $(function () {
         // Deselect all other
         $container.find("input[type=checkbox]").not($this).prop("checked", false);
 
-        const $label = $this.siblings(".form-check-label");
+        const $label = $this.siblings(".filter-checkbox-label");
         const $caret = $container.find(".filter-checkbox-caret");
 
         if ($this.prop("checked")) {  // Move to selected
             $caret.offset($label.offset());
             $caret.css({width: $label.outerWidth(), opacity: 1});
 
-            if ($label.hasClass("form-check-label-yes")) {
+            if ($label.hasClass("filter-checkbox-label-yes")) {
                 $caret.css({backgroundColor: "#63bf69"});
             } else {
                 $caret.css({backgroundColor: "#ff4c2f"});
@@ -31,7 +31,7 @@ function initialMoveCaret() {
     $(".filter-checkbox").find("input[type=checkbox]:checked").each(function () {
         const $this = $(this);
         const $container = $this.parents(".filter-checkbox-container");
-        const $label = $this.siblings(".form-check-label");
+        const $label = $this.siblings(".filter-checkbox-label");
         const $caret = $container.find(".filter-checkbox-caret");
 
         $caret.toggleClass("noanimate", true);
@@ -39,7 +39,7 @@ function initialMoveCaret() {
         $caret.offset($label.offset());
         $caret.css({width: $label.outerWidth(), opacity: 1});
 
-        if ($label.hasClass("form-check-label-yes")) {
+        if ($label.hasClass("filter-checkbox-label-yes")) {
             $caret.css({backgroundColor: "#63bf69"});
         } else {
             $caret.css({backgroundColor: "#ff4c2f"});
