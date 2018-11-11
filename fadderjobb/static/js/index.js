39da361d6389318f1275de9b1772b2adb6a3c71b
Daniel <danro880@student.liu.se>
2018-11-11 23:05:27 +0100

    Mobile design

--- fadderjobb/static/js/index.js
+++ fadderjobb/static/js/index.js
@@ -1,3 +1,7 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+    $('body').toggleClass("mobile", $(document).width() < 992);
+    $(window).on("resize", function () {
+        $('body').toggleClass("mobile", $(document).width() < 992);
+    });
 });
