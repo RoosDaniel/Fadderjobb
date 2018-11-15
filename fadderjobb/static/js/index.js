a8e3f2c4011e2671484ba8d7f594536ca6b4f383
Daniel <danro880@student.liu.se>
2018-11-15 13:41:07 +0100

    Darkmode

--- fadderjobb/static/js/index.js
+++ fadderjobb/static/js/index.js
@@ -4,7 +4,29 @@ $(function () {
 
     $body.toggleClass("mobile", $body.width() < 992);
 
+    updateDarkMode();
+
     $(window).on("resize", function () {
         $body.toggleClass("mobile", $body.width() < 992);
     });
+
+    $('.darkmode-toggler').on("click", function () {
+        console.log(Cookies.get("darkmode") === "true");
+        Cookies.set("darkmode", Cookies.get("darkmode") !== "true");
+
+        updateDarkMode();
+    });
 });
+
+function updateDarkMode() {
+    const $body = $("body");
+    const $toggler = $(".darkmode-toggler");
+
+    if (Cookies.get("darkmode") === "true") {
+        $body.removeClass("darkmode");
+        $toggler.text("Natt");
+    } else {
+        $body.addClass("darkmode");
+        $toggler.text("Dag");
+    }
+}
