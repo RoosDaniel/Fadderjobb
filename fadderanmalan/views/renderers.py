36637bcbfca0fca11149b59057ff6824e105d925
Daniel <danro880@student.liu.se>
2019-02-04 10:29:57 +0100

    Admin can see their profile. Remove users with 0 points from topchart

--- fadderanmalan/views/renderers.py
+++ fadderanmalan/views/renderers.py
@@ -92,7 +92,8 @@ def jobdetails(request, slug):
 
 
 def topchart(request):
-    users = User.objects.annotate(points=Sum("jobs__points")).filter(is_staff=False).order_by("points").all()
+    users = User.objects.annotate(points=Sum("jobs__points"))
+        .filter(points__gte=1).filter(is_staff=False).order_by("points").all()
 
     return render(request, "topchart.html", dict(
         users=users
