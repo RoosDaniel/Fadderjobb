e36c21315ad714251c3758e41219d3dd9e9d952b
Daniel <danro880@student.liu.se>
2019-02-24 22:13:18 +0100

    Add debug-banner

--- fadderjobb/settings.py
+++ fadderjobb/settings.py
@@ -105,6 +105,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'fadderjobb.context_processors.debug'
             ],
         },
     },
