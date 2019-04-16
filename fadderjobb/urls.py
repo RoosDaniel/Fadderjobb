97802ff6b54c497a278d26c4002e20dc5e652062
Daniel <danro880@student.liu.se>
2019-04-16 10:55:36 +0200

    Add a comment

--- fadderjobb/urls.py
+++ fadderjobb/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import cas.views
@@ -27,9 +27,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("fadderanmalan.urls")),
     path('accounts/', include("accounts.urls")),
+
+    # Going to /admin when not logged in results in a redirect to cas.views.login, which I can't be bothered to
+    # find where it's configured. Adding the following line allows me to configure a non-namespaced "cas.views.login"
+    # path which I can redirect to the same login-view as normal.
+    # Meaning: You should _not_ use this path for reversing. If a reverse for loggin in is needed, use the one
+    # configured in the accounts app.
+    path('accounts/login', cas.views.login, name="cas.views.login"),
+
     path('calendar/', include("job_calendar.urls")),
     path('trade/', include("trade.urls")),
-    path('login/', cas.views.login, name="cas.views.login"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
