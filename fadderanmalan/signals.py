06f9a0a2f23e41ce376ff800134c0dff79dda505
Daniel <danro880@student.liu.se>
2019-04-27 14:58:46 +0200

    Remove LQ and EQ instances when JobUsers are created or deleted

--- fadderanmalan/signals.py
+++ fadderanmalan/signals.py
@@ -2,7 +2,7 @@ from django.dispatch import receiver
 from django.db.models.signals import pre_delete, post_save
 from django.db.models import Q
 
-from fadderanmalan.models import JobUser
+from fadderanmalan.models import JobUser, LeaveQueue, EnterQueue
 from trade.models import Trade
 
 
@@ -13,11 +13,19 @@ def _delete_trades(job_user):
         .delete()
 
 
-# TODO Remove EQ and LQ instances
+def _delete_LQ(job_user):
+    LeaveQueue.objects.filter(job=job_user.job, user=job_user.user).delete()
+
+
+def _delete_EQ(job_user):
+    EnterQueue.objects.filter(job=job_user.job, user=job_user.user).delete()
+
 
 @receiver(pre_delete, sender=JobUser)
 def on_deregistration(sender, instance, **kwargs):
     _delete_trades(instance)
+    _delete_LQ(instance)
+    _delete_EQ(instance)  # Technically impossible, but you never know
 
 
 @receiver(post_save, sender=JobUser)
@@ -26,3 +34,5 @@ def on_registration(sender, instance, created, **kwargs):
         return
 
     _delete_trades(instance)
+    _delete_LQ(instance)  # Technically impossible, but you never know
+    _delete_EQ(instance)
