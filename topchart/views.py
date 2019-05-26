671f09bed71323ba8cc254fabcd0c69cb4fc1526
Daniel <danro880@student.liu.se>
2019-05-26 23:40:59 +0200

    Add search to topchart

--- topchart/views.py
+++ topchart/views.py
@@ -8,9 +8,17 @@ User = get_user_model()
 
 
 def index(request):
+    search = request.GET.get("search", "")
+
     users = User.objects.annotate(points=Sum("jobs__points"))\
         .filter(is_staff=False).order_by("points").all()
 
+    if search != "":
+        name_filtered = users.filter(name__icontains=search)
+        username_filtered = users.filter(username__icontains=search)
+        users = name_filtered.union(username_filtered)
+
     return render(request, "topchart/index.html", dict(
         users=users,
+        filter_search=search
     ))
