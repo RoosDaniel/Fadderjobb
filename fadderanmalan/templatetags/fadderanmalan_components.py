c87829fac0fe2430ab42a10e9995ae7d70532175
Daniel <danro880@student.liu.se>
2019-05-26 23:21:04 +0200

    Fix some minor styling bugs

--- fadderanmalan/templatetags/fadderanmalan_components.py
+++ fadderanmalan/templatetags/fadderanmalan_components.py
@@ -3,16 +3,21 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("components/job.html")
-def job_component(job):
-    return {"job": job}
+@register.inclusion_tag("components/job.html", takes_context=True)
+def job_component(context, job):
+    context.update(dict(job=job))
+
+    return context
+
+
+@register.inclusion_tag("components/equipment_ownership.html", takes_context=True)
+def equipment_ownership_component(context, equipment_ownership):
+    context.update(dict(eo=equipment_ownership))
+
+    return context
 
 
 @register.inclusion_tag("components/filter_checkbox.html")
 def filter_checkbox_component(label, id, prev):
-    return {"label": label, "id": id, "prev": prev}
 
-
-@register.inclusion_tag("components/equipment_ownership.html")
-def equipment_ownership_component(equipment_ownership):
-    return {"eo": equipment_ownership}
+    return dict(label=label, id=id, prev=prev)
