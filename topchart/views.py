bb2e6ce7a25a0d41eee4fd2f2af7d6b8741fe52d
Daniel <danro880@student.liu.se>
2019-05-27 20:58:42 +0200

    Topchart now saves placings through search

--- topchart/views.py
+++ topchart/views.py
@@ -13,12 +13,17 @@ def index(request):
     users = User.objects.annotate(points=Sum("jobs__points"))\
         .filter(is_staff=False).order_by("points").all()
 
+    placings = {u.username: placing+1 for placing, u in enumerate(users)}
+
     if search != "":
         name_filtered = users.filter(name__icontains=search)
         username_filtered = users.filter(username__icontains=search)
         users = name_filtered.union(username_filtered)
 
+    usernames = users.values_list('username', flat=True)
+    placings = [placing for username, placing in placings.items() if username in usernames]
+
     return render(request, "topchart/index.html", dict(
-        users=users,
+        users_and_placings=zip(users, placings),
         filter_search=search
     ))
