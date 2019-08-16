e069fdeb1e4bd4cbeb60901b76bb228caeba4e51
Daniel <danro880@student.liu.se>
2019-08-16 20:22:25 +0200

    Patch a few holes where users could see jobs they shouldn't be able to see

--- fadderanmalan/utils/misc.py
+++ fadderanmalan/utils/misc.py
@@ -3,6 +3,8 @@ from ..models import Job
 
 # Removes all jobs that the user cannot view due to insufficient permissions
 def filter_jobs_for_user(user, jobs):
+    original_jobs = jobs[:]
+
     if not user.is_superuser:
         # First, remove all jobs that has a viewing requirement
         jobs = jobs.filter(only_visible_to=None)
@@ -11,6 +13,7 @@ def filter_jobs_for_user(user, jobs):
         if user.is_authenticated:
             for group in user.groups.all():
                 group_jobs = group.jobs.filter(~Job.is_hidden_query_filter())  # Remove hidden jobs
+                group_jobs = group_jobs.filter(id__in=[job.pk for job in original_jobs])
                 jobs = jobs.union(group_jobs)
 
     return jobs
