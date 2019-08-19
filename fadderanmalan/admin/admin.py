358272b9908270840d90f032576ccf4bcf0c53b2
Daniel <danro880@student.liu.se>
2019-08-19 19:23:15 +0200

    Remove sorting of username from EquipmentOwnership in admin
    Shit no work yo

--- fadderanmalan/admin/admin.py
+++ fadderanmalan/admin/admin.py
@@ -235,12 +235,6 @@ class EquipmentOwnershipAdmin(admin.ModelAdmin):
         except EquipmentOwnership.DoesNotExist:
             return {}
 
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        field = super(EquipmentOwnershipAdmin, self).formfield_for_dbfield(db_field, **kwargs)
-        if db_field.name == 'fadder':
-            field.queryset = User.objects.all().order_by('username')
-        return field
-
     def render_change_form(self, request, context, *args, **kwargs):
         context.update({'help_text': ''})
 
