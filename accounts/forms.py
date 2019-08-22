cf600476640e4f71e487b61057052cdd06da6875
Daniel <danro880@student.liu.se>
2019-08-22 16:12:29 +0200

    Add some helptext to edit_profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,14 +7,14 @@ from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget as 
 
 
 class FadderEditForm(forms.Form):
-    email = forms.EmailField(label="Epost", required=True,
+    email = forms.EmailField(label="Epost", required=True, help_text="Epost som du kommer få information skickad till.",
                              widget=forms.EmailInput(attrs={"class": "form-control"}))
 
     name = forms.CharField(label="Namn", required=False, max_length=100, help_text="Vad vill du bli kallad?",
                            widget=forms.TextInput(attrs={"class": "form-control"}))
 
-    phone_number = PhoneNumberField(label="Telefonnummer", required=False,
+    phone_number = PhoneNumberField(label="Telefonnummer", required=False, help_text="Telefonnummer som STABEN kan kontakta dig på.",
                                     widget=PhoneWidget(attrs={"class": "form-control"}))
 
-    motto = forms.CharField(label="Motto", required=False,
-                            widget=forms.Textarea(attrs={"class": "form-control", "maxlength": 100, "rows": 3}))
+    motto = forms.CharField(label="Motto", required=False, help_text="Motto som visas på topplistan.",
+                            widget=forms.Textarea(attrs={"class": "form-control motto-input", "maxlength": 100, "rows": 3}))
